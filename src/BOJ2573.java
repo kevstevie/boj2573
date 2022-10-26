29c61681acd6612c8283913d3b8fbc29a01424c7
kevstevie <jianna442@gmail.com>
2022-10-26 12:11:18 +0900

    feat: 입력받아 maps생성

---
+++ src/BOJ2573.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+class BOJ2573 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        final int N = Integer.parseInt(st.nextToken());
+        final int M = Integer.parseInt(st.nextToken());
+
+        int[][] maps = new int[N][M];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                maps[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+    }
+}
